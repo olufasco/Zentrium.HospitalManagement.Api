1c11b1d45ab858ecbb28b1900b93eb4aecf1c2c0
olufasco <olufasco1@gmail.com>
2025-12-02 11:25:01 +0100

    second

--- Zentrium.HospitalManagement.Api/Models/Ward.cs
+++ Zentrium.HospitalManagement.Api/Models/Ward.cs
@@ -1,6 +1,12 @@
-﻿namespace Zentrium.HospitalManagement.Api.Models
+﻿using Zentrum.HospitalManagement.Api.Models;
+
+namespace Zentrium.HospitalManagement.Api.Models
 {
-    public class Ward
+    public class Ward : BaseEntity
     {
+        public string Name { get; set; } = string.Empty;
+        public string Location { get; set; } = string.Empty;
+        public int Capacity { get; set; } 
+        public ICollection<Bed> Beds { get; set; } = new List<Bed>();
     }
 }
